853ac1021609d0084828e9b30789b35c8d610714
giacomociti <giacomociti@gmail.com>
2015-07-24 20:07:43 +0200

    scripts

--- src/AntaniXml/Script.fsx
+++ src/AntaniXml/Script.fsx
@@ -1,8 +1,12 @@
 // Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library.fs"
+#load "Scripts\load-references.fsx"
+#load "Scripts\load-project.fsx"
 open AntaniXml
 
-let num = Library.hello 42
-printfn "%i" num
+let dir = @"C:\Users\372giaciti\Source\Repos\AntaniXml\tests\AntaniXml.Tests"
+let xsdUri = System.IO.Path.Combine(dir, "po.xsd")
+let gen = XmlElementGenerator.CreateFromSchemaUri(xsdUri, "PurchaseOrder", "")
+
+
---
+++ src/AntaniXml/Scripts/load-project.fsx
@@ -0,0 +1,11 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#load @"load-references.fsx"
+#load @"..\XsdDomain.fs"
+      @"..\XsdFactory.fs"
+      @"..\LexicalMappings.fs"
+      @"..\ConstrainedGenerators.fs"
+      @"..\FacetBasedGenerators.fs"
+      @"..\AtomicGenerators.fs"
+      @"..\XmlGenerator.fs"
+      @"..\XmlElementGenerator.fs"
