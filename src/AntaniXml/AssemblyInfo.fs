da32179fbb408215672279755b0550994b9882bc
giacomociti <giacomociti@gmail.com>
2015-07-26 21:14:19 +0200

    AssemblyInfo

--- src/AntaniXml/AssemblyInfo.fs
+++ src/AntaniXml/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("AntaniXml")>]
 [<assembly: AssemblyProductAttribute("AntaniXml")>]
 [<assembly: AssemblyDescriptionAttribute("xml random generator")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.1.0"
