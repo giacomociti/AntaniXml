09a5252ab27d64563b32ddb98c84bc2c254b573f
giacomociti <giacomociti@gmail.com>
2015-08-30 23:30:52 +0200

    docs

--- docs/content/customizations.fsx
+++ docs/content/customizations.fsx
@@ -1,4 +1,4 @@
-(*** hide ***)
+﻿(*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I @"..\..\bin\AntaniXml"
@@ -9,6 +9,22 @@
 open AntaniXml
 open System.Xml
 open System.Xml.Linq
+
+let xsdText = """
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" 
+        elementFormDefault="qualified" attributeFormDefault="unqualified">
+        <xs:complexType name="barType">
+            <xs:attribute name="id" type="xs:string" use="required" />
+        </xs:complexType>
+        <xs:element name="foo">
+            <xs:complexType>
+	            <xs:sequence>
+                    <xs:element name="bar" type="barType" />
+		            <xs:element name="baz" type="xs:string" />
+	            </xs:sequence>
+            </xs:complexType>
+        </xs:element>
+    </xs:schema>"""
 
 (**
 Customizations
@@ -38,7 +54,6 @@ samples
     xml)      
 |> Seq.iter (printfn "%A") 
     
-
 (**
 
 The same in C# is:
@@ -60,20 +75,21 @@ The above example is just to get the idea. If you don't like imperative code
 you can map the element to another one, but for little fixes the
 mutability allowed in `System.Xml.Linq` may be just fine.
 
-### Providing you own generators
+### Providing your own generators
 
 Allowing custom generators is of course a superior alternative.
 
 To keep things simple, custom generators are allowed only for elements and for global complex types.
 The only way to customize the generation of simple types and attributes is to provide generators for
-the enclosing elements or global complex types. Hopefully this is an acceptable limitation for most practical purposes.
+the enclosing elements or global complex types. 
+Hopefully this is an acceptable limitation for most practical purposes.
 
-FsCheck provides excellent documentation about creating custom generators.
-Anyway there is also an option to provide only a mapping function, so the default generators are used but the generated elements
-are then transformed with a custom function.
+FsCheck provides excellent documentation about creating custom generators, both in F# and in C#.
+Anyway in AntaniXml there is also an option to provide only a mapping function, so the default 
+generators are used but the generated elements are then transformed with a custom function.
 Custom generators are expected to produce `XElement` instances.
-In case of complex types the name of the generated element actually is not important because it will be replaced
-by the proper element name required in each context.
+In case of complex types the name of the generated element actually is not important because it
+will be replaced by the proper element name required in each context.
 
 #### Customizing a global complex type
 The qualified name of the complex type is used as an identifier to pick up a custom generator for all the
@@ -91,10 +107,108 @@ Just beware of (nasty) schemas where two distinct element definitions describe e
 but different content models. In this case a custom generator may not be used because it would apply to
 both element definitions.
 
+Let's give a few concrete examples. In the following we'll be using this schema 
+(assuming a string variable xsdText holds it):
+
+    [lang=xml]
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" 
+        elementFormDefault="qualified" attributeFormDefault="unqualified">
+        <xs:complexType name="barType">
+            <xs:attribute name="id" type="xs:string" use="required" />
+        </xs:complexType>
+        <xs:element name="foo">
+            <xs:complexType>
+	            <xs:sequence>
+                    <xs:element name="bar" type="barType" />
+		            <xs:element name="baz" type="xs:string" />
+	            </xs:sequence>
+            </xs:complexType>
+        </xs:element>
+    </xs:schema>
+
+*)
+
+open FsCheck
+
+let foo = XmlQualifiedName("foo")
+let barType = XmlQualifiedName("barType")
+let toUpper (xml: XElement) = 
+    let id = xml.Attribute(XName.Get("id"))
+    id.Value <- id.Value.ToUpper() 
+    xml
+let cust = CustomGenerators().ForComplexType(barType, toUpper)
+Schema.CreateFromText(xsdText).Arbitrary(foo, cust).Generator
+|> Gen.sample 5 1
+|> Seq.iter (printfn "%A")
+
+(**
+
+The element generated may look like this:
+
+    [lang=xml]
+    <foo>
+      <bar id="FX(-K䤊Q&#xA;N(7RIS.&quot;. />
+      <baz>j慬X"bL@V'yQy[y9O~r~rTH2kwkwkMA+dzdЁdz:$]s</baz>
+    </foo>
+ 
+Yes, text is gibberish (but for testing purposes usually it's a good idea to probe a system
+with strange data), anyway you see the attribute value is all upper case thanks to the customization.
+
+The equivalent C# code is the following:
+
+    var foo = new XmlQualifiedName("foo");
+    var barType = new XmlQualifiedName("barType");
+    var cust = new CustomGenerators()
+        .ForElement(barType, x =>
+        {
+            var id = xml.Attribute("id");
+            id.Value = id.Value.ToUpper();
+            return x;
+        });
+    var arb = Schema.CreateFromText(xsdText).Arbitrary(foo, cust);
+    var samples = Gen.Sample(5, 1, arb.Generator);
+    samples.ToList().ForEach(Console.WriteLine);
+
+The main thing to notice is the `CustomGenerators` class and its method accepting a mapping
+for transforming the generated elements.
+
+*)
+
+let baz = XmlQualifiedName("baz")
+let abcGen = 
+    Gen.elements ["a"; "b"; "c"] 
+    |> Gen.map (fun x -> XElement(XName.Get("baz"), x))
+let cus = CustomGenerators().ForElement(baz, abcGen)
+Schema.CreateFromText(xsdText).Arbitrary(foo, cus).Generator
+|> Gen.sample 5 2
+|> Seq.iter (printfn "%A")
+    
+
+(**
+    In the example above instead we are creating a generator from scratch using the FsCheck combinators.
+    Now `baz` is no more gibberish because we took full control of the random generation.
+
+    [lang=xml]
+    <foo>
+      <bar id=",B{e{e{;%;t^t^t4y4mWmWm-r-fPfPf&amp;k&amp;_" />
+      <baz>b</baz>
+    </foo>
+    <foo>
+      <bar id="ZDZB}B}BS=S５v;v;L玲o4o4o&#xA;E/h~h~h(&gt;(awawa!7!ZpZpZu0:SiSiSn)bLbLbLv[/-&#x9;|cFh!;EBog7c" />
+      <baz>c</baz>
+    </foo>
 
 
+Here's the C# version of the same example:
 
-
+    var foo = new XmlQualifiedName("foo");
+    var abcGen = Gen.Elements(new[] { "a", "b", "c" })
+        .Select(x => new XElement("baz", x));
+    var baz = new XmlQualifiedName("baz");
+    var cus = new CustomGenerators().ForElement(baz, abcGen);
+    var arb = Schema.CreateFromText(xsdText).Arbitrary(foo, cus);
+    var samples = Gen.Sample(5, 2, arb.Generator);
+    samples.ToList().ForEach(Console.WriteLine);
 
 
 
