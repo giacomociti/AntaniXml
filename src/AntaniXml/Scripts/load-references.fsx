93778c5895b963f23c5936ac14c63c3b148e802c
giacomociti <giacomociti@gmail.com>
2015-10-24 13:41:57 +0200

    removed c# proj from main solution

--- src/AntaniXml/Scripts/load-references.fsx
+++ src/AntaniXml/Scripts/load-references.fsx
@@ -1,9 +1,9 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#r @"..\..\..\packages\Fare\lib\net35\Fare.dll"
-#r @"..\..\..\packages\FsCheck\lib\net45\FsCheck.dll"
-#r @"System.Core.dll"
-#r @"System.dll"
-#r @"System.Numerics.dll"
-#r @"System.Xml.dll"
-#r @"System.Xml.Linq.dll"
+#r "../../../packages/Fare/lib/net35/Fare.dll"
+#r "../../../packages/FsCheck/lib/net45/FsCheck.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "System.Xml.dll"
+#r "System.Xml.Linq.dll"
