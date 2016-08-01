4e51e98aef40ab150a33eb6b93c4b408a758c04d
giacomociti <giacomociti@gmail.com>
2016-08-01 22:48:42 +0200

    updated refrences

--- src/AntaniXml/AssemblyInfo.fs
+++ src/AntaniXml/AssemblyInfo.fs
@@ -4,9 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("AntaniXml")>]
 [<assembly: AssemblyProductAttribute("AntaniXml")>]
 [<assembly: AssemblyDescriptionAttribute("XML random generator")>]
-[<assembly: AssemblyVersionAttribute("0.7.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.7.1")>]
+[<assembly: AssemblyVersionAttribute("0.7.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.7.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.7.1"
+    let [<Literal>] Version = "0.7.2"
+    let [<Literal>] InformationalVersion = "0.7.2"
